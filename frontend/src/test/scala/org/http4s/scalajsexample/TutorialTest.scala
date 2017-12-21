a45948f8edc269f3c2ef7f108fa51f557f168b08
Christopher Davenport <Chris@ChristopherDavenport.tech>
2017-12-20 22:47:32 -0500

    Fix Unused Import in tests

--- frontend/src/test/scala/org/http4s/scalajsexample/TutorialTest.scala
+++ frontend/src/test/scala/org/http4s/scalajsexample/TutorialTest.scala
@@ -1,12 +1,10 @@
 package org.http4s.scalajsexample
 
-import org.scalajs.dom
-import dom.document
 import utest._
 
 object TutorialTest extends TestSuite {
 
-  def tests = TestSuite {
+  def tests = Tests {
     'HelloWorld {
       assert("Hello World" == "Hello World")
     }
